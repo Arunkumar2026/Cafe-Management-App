ea3b0d91f1a760036e024344601d5be91523578d
Arun Kumar <akulakumar453@gmail.com>
2026-01-23 11:01:24 +0530

    This is Electricity Bill Generator Python file

---
+++ electricityBill.py
@@ -0,0 +1,14 @@
+#This is Electricity Bill Generator program
+
+def eletriityBill(units):
+    if units <= 100:
+        bill = units * 5
+    elif (units <= 200):
+        bill = (100 * 5) + ((units - 100) * 7)
+    elif (units <= 300):
+        bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+    else:
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 15)
+    return bill 
+units = int(input("Enter your UNITS: "))
+print(eletriityBill(units))
